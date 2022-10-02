8adf93e4223e117a3e7bff5bb5932de7bc860e9c
SOME1HING <yashprakash2005@gmail.com>
2022-10-03 01:36:24 +0530

    Update config.py

--- Yumeko/config.py
+++ Yumeko/config.py
@@ -15,10 +15,10 @@ class Config(object):
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
     API_ID = 8477975  # integer value, dont use "6973446"
-    API_HASH = "c093e068dc7c42309ccc2de125b6c1b3"
-    TOKEN = "5278580353:AAFXFnQ-cfhdeUB-U0oUkgpiyqa9TUjApRU"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = "1635151800"  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sneha_UwU_OwO"
+    API_HASH = "jhj"
+    TOKEN = "faddas"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = "32324"  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "daf"
     SUPPORT_CHAT = "tyranteyeeee"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
         -1001546153360
@@ -34,7 +34,7 @@ class Config(object):
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = "29sw~FuIbphLGXWd38AYb6VnlJ86DKKfnjWHS91VlpUUSyzpHoVlZWxQ0F4P_ZTy"  # go to support.spamwat.ch to get key
+    SPAMWATCH_API = "29sw~DSF"  # go to support.spamwat.ch to get key
     SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
 
     # OPTIONAL
@@ -59,9 +59,9 @@ class Config(object):
     BAN_STICKER = ""  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
     ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
     CASH_API_KEY = (
-        "V7NS1NBFEL4X24L6"  # Get your API key from https://www.alphavantage.co/support/#api-key
+        "ds"  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
-    TIME_API_KEY = "2AS711XS1O9B"  # Get your API key from https://timezonedb.com/api
+    TIME_API_KEY = "S"  # Get your API key from https://timezonedb.com/api
     WALL_API = (
         "awoo"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
     )
